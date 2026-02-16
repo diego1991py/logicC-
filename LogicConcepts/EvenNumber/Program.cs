00a1f99c9c6fcd9a5c72ab96a032c8d41460697e
Diego Valencia <91valenciadiego@gmail.com>
2026-02-15 21:34:59 -0500

    Program to determine if a value is even or odd, prevents closures due to input errors.

---
+++ LogicConcepts/EvenNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+Console.Write("Ingrese un número o ingrese S para salir: ");
+
+var number = Console.ReadLine();
+
+if (int.TryParse(number, out int numberInt))
+{
+    if (numberInt % 2 == 0)
+    {
+        Console.WriteLine($"El numero {numberInt} es par");
+    }
+    else 
+    { 
+        Console.WriteLine($"El número {numberInt} no es par"); 
+    }
+}
+else
+{
+    Console.WriteLine($"Por favor ingresar solo números");
+}
+
