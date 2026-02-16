ea7880f09a29ec56b5f64b0da5a46de55a0beceb
Diego Valencia <91valenciadiego@gmail.com>
2026-02-15 22:58:12 -0500

    Write a program that prompts the user to enter three numbers and then displays the largest, middle, and smallest of the entered numbers.

---
+++ LogicConcepts/OrderNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿int counter = 1;
+
+List<int> numberList = new List<int>();
+
+while (counter <= 3)
+{
+
+    Console.Write("Ingrese un número: ");
+
+    var number = Console.ReadLine();
+
+    if (int.TryParse(number, out int newNumber))
+    {
+        numberList.Add(newNumber);
+        counter++;
+    }
+
+    else { Console.WriteLine("Ingresar solo números"); }
+
+}
+
+numberList.Sort();
+numberList.Reverse();
+
+
+for (int i = 0; i < 1; i++)
+{
+    Console.WriteLine($"El número {numberList[0]} es el mayor");
+    Console.WriteLine($"El número {numberList[1]} es el del medio");
+    Console.WriteLine($"El número {numberList[2]} es el menor");
+
+}
