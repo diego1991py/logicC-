0f35a147b4ad98d6ff2592b2c59c1758c19ea608
Diego Valencia <91valenciadiego@gmail.com>
2026-02-16 22:12:09 -0500

    Write a program that asks for two numbers and says if the second is a multiple of the first.

---
+++ LogicConcepts/Multiples/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Ingrese un número: ");
+
+var number_a = Console.ReadLine();
+
+var number_b = Console.ReadLine();
+
+if (int.TryParse(number_a, out int first_number) && int.TryParse(number_b, out int second_number))
+{
+    if (first_number % second_number == 0)
+    {
+        Console.WriteLine($"El numero {first_number} es multiplo de {second_number}");
+    }
+    else { Console.WriteLine($"El numero {first_number} no es multiplo de {second_number}"); }
+}
+
+else
+{
+    Console.WriteLine("Por favor ingresar solo números");
+}
+
+
+
