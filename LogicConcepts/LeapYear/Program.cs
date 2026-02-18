56d19878c4bb2df9b573162d5b5c62ddbc25fd69
Diego Valencia <91valenciadiego@gmail.com>
2026-02-17 23:27:42 -0500

    Create a program that asks for an employee's name, their basic hourly wage, and the number of hours worked in the month; print their name and monthly salary if it is above the minimum wage, otherwise print only the name.

---
+++ LogicConcepts/LeapYear/Program.cs
@@ -0,0 +1,68 @@
+﻿
+bool IsLeapYear(int year)
+{
+    if(year % 400 == 0)
+    {
+        return true;
+    }
+    else if(year % 100 == 0)
+    {
+        return false;
+    }
+    else if(year % 4 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+
+//Console.WriteLine("Hola, por favor ingresar el año o s para salir");
+
+var numberString = String.Empty;
+
+do
+{
+    Console.WriteLine("Hola, por favor ingresar el año o s para salir");
+    numberString = Console.ReadLine().ToLower();
+
+
+    if(numberString == "s")
+    {
+        break;
+    }
+  
+    if (int.TryParse(numberString, out int year))
+    {
+        if (IsLeapYear(year))
+        {
+            Console.WriteLine($"El año {year} es bisiesto");
+        }
+        else
+        {
+            Console.WriteLine($"El año {year} no es bisiesto");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Ingresar solo numeros");
+
+
+}
+while (numberString != "s");
+
+Console.WriteLine("Gracias");
+
+
+
+
+
+
+
+
+
+
