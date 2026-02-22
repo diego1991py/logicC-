94afb6c17568dac40e7a18affcc3d08404c25626
Diego Valencia <91valenciadiego@gmail.com>
2026-02-22 16:46:14 -0500

    A desk store offers the following discounts: if the customer buys fewer than 5 units, they receive a 10% discount on the purchase; if the number of units is greater than or equal to five but less than 10, they receive a 20% discount; and if they buy 10 or more, they receive a 40% discount. Write a program that determines how much a customer must pay if the price of each desk is $650,000.

---
+++ LogicConcepts/Sales.Backend/Sale.cs
@@ -0,0 +1,67 @@
+﻿namespace Sales.Backend
+{
+    public class Sale
+    {
+        private int _amount;
+        private const double _price = 650000;
+
+
+        public Sale(int amount)
+        {
+            Amount = amount;
+        }
+
+        public int Amount
+        {
+            get => _amount;
+            set => _amount = SaveQuantity(value);
+        }
+
+        private double Price
+        {
+            get => _price;
+        }
+
+        public double SubTotal
+        {
+            get => CalculateSubtotal();
+        }
+
+        public double FinalPrice
+        {
+            get => CalculateFinalPrice();
+        }
+
+        public override string ToString()
+        {
+            return $"Número de escritorios: {Amount}\nEl valor a pagar es: {FinalPrice:C}";
+        }
+
+        private int SaveQuantity( int amount)
+        {
+            return amount;
+        }
+
+        private double CalculateSubtotal()
+        {
+            return Amount * Price;
+        }
+
+        private double CalculateFinalPrice()
+        {
+            if (Amount < 5)
+            {
+                return SubTotal - (SubTotal * 0.10);
+            }
+            else if (Amount < 10)
+            {
+                return SubTotal - (SubTotal * 0.20);
+            }
+            else
+            {
+                return SubTotal - (SubTotal * 0.40);
+            }
+        }
+
+    }
+}
---
+++ LogicConcepts/Sales.Frontend/Program.cs
@@ -0,0 +1,64 @@
+﻿using Sales.Backend;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string option = InitialOption();
+        while (option != "")
+        {
+
+            if (option == "a")
+            {
+                Console.WriteLine("Bienvenido");
+                ShowMenu();
+            }
+            else if (option == "s")
+            {
+                Console.WriteLine($"Gracias, vuelva pronto");
+                break;
+            }
+            else
+            {
+                Console.WriteLine($"La opción {option} no es valida");
+            }
+            option = InitialOption();
+        }
+
+    }
+
+    static string InitialOption()
+    {
+        Console.WriteLine("Por favor ingresar a para ingresar datos o ingresar s para salir");
+        string option = Console.ReadLine().ToLower();
+        if (string.IsNullOrWhiteSpace(option))
+        {
+            Console.WriteLine("El campo no debe de estar vacio");
+            option = InitialOption();
+        }
+        return option;
+    }
+
+    static void ShowMenu()
+    {
+        try
+        {
+            Console.WriteLine("Por favor ingresar cantidad de escritorios");
+            var stringAmount = Console.ReadLine();
+
+            if (!int.TryParse(stringAmount, out int amount))
+            {
+                Console.WriteLine("Ingresar solo valores númericos");
+                return;
+            }
+            var sale = new Sale(amount);
+            Console.WriteLine(sale);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+
+        }
+    }
+}
