9bf5dc384e078cfb96b8aebfcf6129b5327922ea
Diego Valencia <91valenciadiego@gmail.com>
2026-02-22 22:20:00 -0500

    Se desea obtener el valor de la matrícula de un estudiante

---
+++ LogicConcepts/Student.Backend/Enrollment.cs
@@ -0,0 +1,140 @@
+﻿namespace Student.Backend
+{
+    public class Enrollment
+    {
+        private int _numberCredits;
+        private double _valueCredits;
+        private int _stratum;
+        private const int _creditLimit = 20;
+        public Enrollment(int numberCredits, double valueCredits, int stratum)
+        {
+            NumberCredits = numberCredits;
+            ValueCredits = valueCredits;
+            Stratum = stratum;
+
+        }
+
+        public int NumberCredits
+        {
+            get => _numberCredits;
+            set => _numberCredits = GetNumberCredis(value);
+        }
+
+        public double ValueCredits
+        {
+            get => _valueCredits;
+            set => _valueCredits = GetValueCredits(value);
+        }
+
+        public double BaseCost
+        {
+            get => CalculateBaseCost();
+        }
+
+        public int Stratum
+        {
+            get => _stratum;
+            set => _stratum = GetStratum(value);
+
+        }
+
+        public double FinalCost
+        {
+            get => CalculateFinalCost();
+        }
+
+        private int LimitCredit
+        {
+            get => _creditLimit;
+        }
+
+
+        public int Subsidy
+        {
+            get => GetSubsidy(Stratum);
+        }
+
+        public override string ToString()
+        {
+            return $"Número de créditos: {NumberCredits}\nValor crédito: {ValueCredits:C}\nEstrato del estudiante: {Stratum}\n" +
+                $"Costo de la matricula: {FinalCost:C}\nValor del subsidio: {Subsidy:C}";
+        }
+
+
+
+        private int GetNumberCredis(int numberCredits)
+        {
+            if (numberCredits <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numberCredits), "El número de créditos debe de ser mayor a 0");
+            }
+            return numberCredits;
+        }
+
+        private double GetValueCredits(double valueCredits)
+        {
+            if (valueCredits <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(valueCredits), $"El valor de los créditos debe de ser mayor a {0}");
+            }
+            return valueCredits;
+        }
+
+        public double CalculateBaseCost()
+        {
+            if (NumberCredits <= 20)
+            {
+                return NumberCredits * ValueCredits;
+            }
+            else
+            {
+                return (LimitCredit * ValueCredits) + (((NumberCredits - LimitCredit) * (ValueCredits * 2) ));
+            }
+            
+        }
+
+        private int GetStratum(int stratum)
+        {
+            if (stratum <= 0 || stratum >= 6)
+            {
+                throw new ArgumentOutOfRangeException(nameof(stratum), $"El valor del estrato debe estar entre 1 y 6");
+            }
+            return stratum;
+        }
+
+        private double CalculateFinalCost()
+        {
+            switch (Stratum)
+            {
+                case 1:
+                    return BaseCost - (BaseCost * 0.80);
+
+                case 2:
+                    return BaseCost - (BaseCost * 0.50);
+
+                case 3:
+                    return BaseCost - (BaseCost * 0.30);
+
+                default:
+                    return BaseCost;
+            }
+        }
+
+        private int GetSubsidy(int stratum)
+        {
+            if (stratum == 1)
+            {
+                return 200000;
+            }
+            else if (stratum == 2)
+            {
+                return 200000;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+}
---
+++ LogicConcepts/Student.Frontend/Program.cs
@@ -0,0 +1,83 @@
+﻿using Student.Backend;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string option = InitialOption();
+        while (option != "")
+        {
+
+            if (option == "a")
+            {
+                Console.WriteLine("Bienvenido");
+                ShowMenu();
+            }
+            else if (option == "s")
+            {
+                Console.WriteLine($"Gracias, vuelva pronto");
+                break;
+            }
+            else
+            {
+                Console.WriteLine($"La opción {option} no es valida");
+            }
+            option = InitialOption();
+        }
+
+    }
+
+    static string InitialOption()
+    {
+        Console.WriteLine("Por favor ingresar a para ingresar datos o ingresar s para salir");
+        string option = Console.ReadLine().ToLower();
+        if (string.IsNullOrWhiteSpace(option))
+        {
+            Console.WriteLine("El campo no debe de estar vacio");
+            option = InitialOption();
+        }
+        return option;
+    }
+
+    static void ShowMenu()
+    {
+        try
+        {
+            Console.WriteLine("Por favor ingresar número de créditos");
+            var stringCredits = Console.ReadLine();
+
+            if (!int.TryParse(stringCredits, out int numberCredits))
+            {
+                Console.WriteLine("Ingresar solo valores númericos");
+                return;
+            }
+
+            Console.WriteLine("Por favor ingresar valor de los créditos");
+            var stringvalueCredits = Console.ReadLine();
+
+            if (!int.TryParse(stringvalueCredits, out int valueCredits))
+            {
+                Console.WriteLine("Ingresar solo valores númericos");
+                return;
+            }
+
+            Console.WriteLine("Por favor ingresar el estrato");
+            var stringStratum = Console.ReadLine();
+
+            if (!int.TryParse(stringStratum, out int stratum))
+            {
+                Console.WriteLine("Ingresar solo valores númericos");
+                return;
+            }
+            var enrollment = new Enrollment(numberCredits, valueCredits, stratum);
+            Console.WriteLine(enrollment);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+
+        }
+    }
+}
+
